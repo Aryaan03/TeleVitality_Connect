4f5ca05ed285a404de1d4543d1d1e797f999490a
Aryaan Shaikh <57898150+Aryaan03@users.noreply.github.com>
2025-02-07 19:43:48 -0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Navigation from './components/Navigation'
+import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import Dashboard from './pages/Dashboard';
-import Navigation from './components/Navigation';
+import Contact from './pages/Contact';
 import ForgotPassword from './pages/ForgotPassword';
+
 
 // Main app component
 function App() {
@@ -11,15 +14,16 @@ function App() {
     <BrowserRouter>
       <Navigation />
       <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/" element={<Navigation />}/>
+        <Route index element={<HomePage />} /> 
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="/forgot-password" element={<ForgotPassword/>}/>
       </Routes>
     </BrowserRouter>
   );
 }
 
-// Add this export at the end
 export default App;
