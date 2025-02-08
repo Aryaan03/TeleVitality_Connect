5720343264ee347b1a97e276b3803a7c36020a8f
Yasir Khan <yasirk.172000@gmail.com>
2025-02-07 23:39:06 -0500

    Working backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,6 @@ import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import Dashboard from './pages/Dashboard';
 import Contact from './pages/Contact';
-import { AuthProvider } from './src/contexts/AuthContext';
 
 
 
@@ -19,11 +18,7 @@ function App() {
         <Route index element={<HomePage />} /> 
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/dashboard" element={
-          <PrivateRoute>
-            <Dashboard />
-          </PrivateRoute>
-        } />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
