bdb0cb1efceaf050954d2611436be37ac0dacf84
Aryaan Shaikh <57898150+Aryaan03@users.noreply.github.com>
2025-02-07 19:39:37 -0500

    Update Navigation.jsx

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,12 +1,21 @@
-import { Link } from 'react-router-dom';
-import { Button, Stack } from '@mui/material';
+import { Link, Outlet } from 'react-router-dom';
+import { Button, Stack, Paper, Box } from '@mui/material';
 
 export default function Navigation() {
   return (
-    <Stack direction="row" spacing={2} sx={{ p: 2 }}>
-      <Button component={Link} to="/login">Login</Button>
-      <Button component={Link} to="/register">Register</Button>
-      <Button component={Link} to="/dashboard">Dashboard</Button>
-    </Stack>
+    <Box>
+      {/* Top Navigation Bar */}
+      <Paper elevation={3} sx={{ p: 2, borderRadius: 0 }}>
+        <Stack direction="row" spacing={2} justifyContent="flex-end">
+          <Button component={Link} to="/">Home</Button>
+          <Button component={Link} to="/login">Login</Button>
+          <Button component={Link} to="/register">Register</Button>
+          <Button component={Link} to="/contact">Contact</Button>
+        </Stack>
+      </Paper>
+
+      {/* Outlet for rendering nested routes */}
+      <Outlet />
+    </Box>
   );
 }
